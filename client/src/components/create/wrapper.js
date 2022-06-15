d1b9746e6c6791faa141ad97c30bd4ad63bb4e9a
BeeImp <zero9231@naver.com>
2022-06-15 10:43:54 +0900

    Style: create

--- client/src/components/create/wrapper.js
+++ client/src/components/create/wrapper.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
-  const Wapper = styled.div`
-    max-width: 1100px;
-
-  `;
+const Wapper = styled.div`
+  max-width: 1100px;
+  padding: 25px;
+  box-sizing: border-box;
+`;
 const CreateWrapper = ({ children }) => {
   return (
     <Wapper>
