9b059287b5ae673a494f70fae6c2e020d365bc59
hae jeong yu <haejeonghy@gmail.com>
2022-06-15 17:43:49 +0900

    Feat: nft list useState로 관리
    get nftlist 유틸로 분리
    mypage는 props로 nftlist 받아서 갤러리로 전달

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,21 @@ import MyPage from './routes/MyPage';
 import Home from './routes/Home';
 import { Menubar } from './components/export';
 import Create from './routes/Create';
+import {useState} from 'react'
 
 function App() {
+
+  const [nftList, setNftList] = useState([])
+  const [address, setAddress] = useState('')
   
   return (
     <div className="App">
       <Menubar></Menubar>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/' element={<Home nftList={nftList} setNftList={setNftList} address={address} setAddress={setAddress}></Home>}></Route>
         <Route path='/detail' element={<div>Detail</div>}></Route>
         <Route path='/create' element={<Create></Create>}></Route>
-        <Route path='/mypage' element={<MyPage></MyPage>}></Route>
+        <Route path='/mypage' element={<MyPage nftList={nftList} address={address}></MyPage>}></Route>
       </Routes>
     </div>
   );
