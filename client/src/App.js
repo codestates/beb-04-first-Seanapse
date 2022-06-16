d4de7e3ae229aa2c67e6a0c73c6166bae8d13c47
BeeImp <zero9231@naver.com>
2022-06-16 12:33:06 +0900

    Feat: detail params 라우팅 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MyPage from './routes/MyPage';
 import Home from './routes/Home';
 import { Menubar } from './components/export';
@@ -9,16 +9,16 @@ import { useState } from 'react';
 
 function App() {
 
-  const [nftList, setNftList] = useState([])
-  const [address, setAddress] = useState('')
-  const [searchKeyword, setSearchKeyword]= useState('');
+  const [nftList, setNftList] = useState([]);
+  const [address, setAddress] = useState('');
+  const [searchKeyword, setSearchKeyword] = useState('');
 
   return (
     <div className="App">
-      <Menubar setSearchKeyword={setSearchKeyword} address={address} setAddress={setAddress}/>
+      <Menubar setSearchKeyword={setSearchKeyword} address={address} setAddress={setAddress} />
       <Routes>
         <Route path='/' element={<Home searchKeyword={searchKeyword} nftList={nftList} setNftList={setNftList} address={address} setAddress={setAddress}></Home>}></Route>
-        <Route path='/detail' element={<Detail></Detail>}></Route>
+        <Route path='/detail/:tokenId' element={<Detail></Detail>}></Route>
         <Route path='/create' element={<Create></Create>}></Route>
         <Route path='/mypage' element={<MyPage nftList={nftList} address={address}></MyPage>}></Route>
       </Routes>
